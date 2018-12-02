2badbec6b26580722e1bf46ea8ca5aeb026d8925
Dominic Freeston <dominic.freeston@gmail.com>
2018-12-02 22:26:59 +0000

    Day 1 of 2018 in Clojure

---
+++ 2018/1/clojure/core.clj
@@ -0,0 +1,12 @@
+(ns advent.2018.1)
+
+(defn find-repeat-freq [input]
+  (loop [freq 0 found #{} nums (cycle input)]
+    (let [new-freq (+ freq (first nums))]
+      (if (contains? found new-freq)
+        new-freq
+        (recur new-freq (conj found new-freq) (rest nums))))))
+
+(def nums (map read-string *command-line-args*))
+(println (reduce + nums))
+(println (find-repeat-freq nums))
